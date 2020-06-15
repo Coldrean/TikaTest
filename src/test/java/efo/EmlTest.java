ec9cade22ceb2d2669968e7c1a2f17cdf9e5ceec
user <liangyu@xxy.com>
2020-06-15 11:25:28 +0800

    可以将邮件的正文和附件分开提取

--- src/test/java/efo/EmlTest.java
+++ src/test/java/efo/EmlTest.java
@@ -1,12 +1,19 @@
 package efo;
 
+import efo.extractor.ExtractEmbeddedFiles;
 import efo.handlers.MyHandler;
 import org.apache.tika.exception.TikaException;
+import org.apache.tika.extractor.ParsingEmbeddedDocumentExtractor;
 import org.apache.tika.metadata.Metadata;
+import org.apache.tika.metadata.TikaCoreProperties;
 import org.apache.tika.parser.AutoDetectParser;
-import org.apache.tika.sax.BodyContentHandler;
-import org.apache.tika.sax.LinkContentHandler;
-import org.apache.tika.sax.ToXMLContentHandler;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.parser.RecursiveParserWrapper;
+import org.apache.tika.sax.*;
+import org.apache.tika.sax.xpath.Matcher;
+import org.apache.tika.sax.xpath.MatchingContentHandler;
+import org.apache.tika.sax.xpath.XPathParser;
 import org.junit.Test;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
@@ -15,14 +22,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class EmlTest {
+
     AutoDetectParser autoParser = new AutoDetectParser();
     ContentHandler toXMLContentHandler = new ToXMLContentHandler();
     BodyContentHandler bodyContentHandler = new BodyContentHandler();
     MyHandler myHandler=new MyHandler();
     Metadata metadata = new Metadata();
-
+    String RESOURCE_NAME_KEY = "resourceName";
     String inputDir="./test-files";
     String outputDir="./result";
     @Test
@@ -42,6 +51,7 @@ public class EmlTest {
     public void testTikaParseEmlWithDocAttachment__output_txt() throws TikaException, SAXException, IOException {
         String filename="eml-with-doc-attachment-test.eml";
         InputStream stream =this.getClass().getClassLoader().getResourceAsStream(Paths.get(inputDir,filename).toString());
+
         autoParser.parse(stream, bodyContentHandler, metadata);
         System.out.println(bodyContentHandler.toString());
         System.out.println(metadata.toString());
@@ -50,12 +60,49 @@ public class EmlTest {
 
     @Test
     public void testMyHandlerParseEmlWithDocAttachment__output_txt() throws TikaException, SAXException, IOException {
-        String filename="eml-with-doc-attachment-test.eml";
+        String filename="word-with-image-test.doc";
         InputStream stream =this.getClass().getClassLoader().getResourceAsStream(Paths.get(inputDir,filename).toString());
-        autoParser.parse(stream, myHandler, metadata);
-        System.out.println(myHandler.toString());
-        System.out.println(metadata.toString());
-        Files.write(Paths.get(outputDir,filename.split("\\.")[0]+".txt"),myHandler.toString().getBytes());
+        ExtractEmbeddedFiles extractEmbeddedFiles=new ExtractEmbeddedFiles();
+        extractEmbeddedFiles.extract(stream,Paths.get(outputDir));
     }
+
+    @Test
+    public void testTikaParseEmlWithTwoDocAttachment_output_html() throws TikaException, SAXException, IOException {
+        String filename="eml-with-two-attachments.eml";
+
+        XPathParser xhtmlParser = new XPathParser("http://www.w3.org/1999/xhtml", XHTMLContentHandler.XHTML);
+        ////div[@class='email-entry']
+        Matcher divContentMatcher = xhtmlParser.parse("html[@xmlns='http://www.w3.org/1999/xhtml']");
+        ContentHandler handler = new MatchingContentHandler(
+                new ToXMLContentHandler(), divContentMatcher);
+
+        AutoDetectParser parser = new AutoDetectParser();
+        Metadata metadata = new Metadata();
+        try (InputStream stream =this.getClass().getClassLoader().getResourceAsStream(Paths.get(inputDir,filename).toString());) {
+            parser.parse(stream, handler, metadata);
+            System.out.printf(handler.toString());
+        }
+    }
+
+    @Test
+    public void recursiveParserWrapperExample() throws IOException,
+            SAXException, TikaException {
+        String filename="eml-with-two-attachments.eml";
+        Parser p = new AutoDetectParser();
+        ContentHandlerFactory factory = new BasicContentHandlerFactory(
+                BasicContentHandlerFactory.HANDLER_TYPE.TEXT, -1);
+
+        RecursiveParserWrapper wrapper = new RecursiveParserWrapper(p);
+        Metadata metadata = new Metadata();
+        metadata.set(RESOURCE_NAME_KEY, "test_recursive_embedded.docx");
+        ParseContext context = new ParseContext();
+        RecursiveParserWrapperHandler handler = new RecursiveParserWrapperHandler(factory, -1);
+        try (InputStream stream =this.getClass().getClassLoader().getResourceAsStream(Paths.get(inputDir,filename).toString())) {
+            wrapper.parse(stream, handler, metadata, context);
+        }
+        System.out.printf(handler.getMetadataList().get(0).get(""));
+        //return handler.getMetadataList();
+    }
+
 
 }
--- src/test/java/efo/PdfTest.java
+++ src/test/java/efo/PdfTest.java
@@ -1,12 +1,16 @@
 package efo;
 
+import efo.extractor.ExtractEmbeddedFiles;
 import efo.handlers.MyHandler;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.pdf.PDFParser;
+import org.apache.tika.parser.pdf.PDFParserConfig;
 import org.apache.tika.sax.BodyContentHandler;
 import org.apache.tika.sax.LinkContentHandler;
 import org.apache.tika.sax.ToXMLContentHandler;
+import org.junit.Before;
 import org.junit.Test;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
@@ -17,13 +21,24 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class PdfTest {
-    AutoDetectParser autoParser = new AutoDetectParser();
+    //AutoDetectParser autoParser = new AutoDetectParser();
+    PDFParser autoParser=new PDFParser();
+    PDFParserConfig config=new PDFParserConfig();
+
+
     ContentHandler contentHandler = new ToXMLContentHandler();
     BodyContentHandler bodyContentHandler = new BodyContentHandler();
     LinkContentHandler linkContentHandler=new LinkContentHandler();
     Metadata metadata = new Metadata();
     String inputDir="./test-files";
     String outputDir="./result";
+
+    @Before
+    public void init(){
+        config.setExtractInlineImages(true);
+        config.setExtractUniqueInlineImagesOnly(true);
+        autoParser.setPDFParserConfig(config);
+    }
 
     @Test
     public void testTikaParsePdfWithImage_output_html() throws TikaException, SAXException, IOException {
@@ -76,6 +91,14 @@ public class PdfTest {
         System.out.println(myHandler.toString());
         System.out.println(metadata.toString());
         Files.write(Paths.get(outputDir,filename.split("\\.")[0]+".txt"),myHandler.toString().getBytes());
+    }
+
+    @Test
+    public void testMyHandlerParsePdfWithImgs__output_imgs() throws TikaException, SAXException, IOException {
+        String filename="pdf-text-with-image.pdf";
+        InputStream stream =this.getClass().getClassLoader().getResourceAsStream(Paths.get(inputDir,filename).toString());
+        ExtractEmbeddedFiles extractEmbeddedFiles=new ExtractEmbeddedFiles();
+        extractEmbeddedFiles.extract(stream,Paths.get(outputDir));
     }
 
 }
